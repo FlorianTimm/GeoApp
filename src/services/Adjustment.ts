67099b94098390ae1515d9c3d6e24b49b6b982bd
FlorianTimm <FlorianTimm@github.com>
2025-01-05 12:49:49 +0100

    AusgleichsFilterFunktion vorbereitet

--- src/services/Adjustment.ts
+++ src/services/Adjustment.ts
@@ -2,7 +2,7 @@ import { Measurement } from '@/types/Measurement';
 import { Point } from '@/types/Point';
 import { TheodoliteMeasure } from '@/types/TheodoliteMeasure';
 import { azimuth, gonBetween0And400, gonBetweenMinus200And200, gonToRad, zenithDistance } from '@/utils';
-import { inv, transpose, multiply, subtract, diag, add, norm, ones, MathNumericType, Matrix, i, size, sqrt, divide } from 'mathjs';
+import { inv, transpose, multiply, subtract, diag, add, norm, ones, MathNumericType, Matrix, i, size, sqrt, divide, string, sum, boolean } from 'mathjs';
 
 export class Adjustment {
     private measurements: Measurement[];
@@ -22,12 +22,17 @@ export class Adjustment {
 
     private RHO = 200 / Math.PI;
 
-    private x_measurements: { [id: number]: { o?: number, ih?: number } } = {};
-    private l_measurements: { [id: number]: { [id: number]: { hz?: number, v?: number, dist?: number } } } = {};
-    private x_points: { [nr: string]: { x?: number, y?: number, z?: number } } = {};
-    private l_points: { [nr: string]: { x?: number, y?: number, z?: number } } = {};
-    private l_angles: number[] = [];
-    private x_angles: number[] = [];
+    private x_measurements: { measure: number, component: 'o' | 'ih' }[] = [];
+    private x_measurements_pos: { [measure: number]: { o?: number, ih?: number } } = {};
+    private l_measurements: { measure: number, entry: number, component: 'hz' | 'v' | 'dist' }[] = [];
+    private x_points: { nr: string, component: 'x' | 'y' | 'z' }[] = [];
+    private x_points_pos: { [nr: string]: { x?: number, y?: number, z?: number } } = {};
+    private l_points: { nr: string, component: 'x' | 'y' | 'z' }[] = [];
+    private l_isAngle: boolean[] = [];
+    private x_isAngle: boolean[] = [];
+
+    private x_filter: boolean[] = [];  // true: adjustment for value is impossible
+    private l_filter: boolean[] = [];  // true: adjustment for value is impossible
 
     constructor(measurements: Measurement[], points: { [nr: string]: Point }) {
         this.measurements = measurements;
@@ -45,28 +50,38 @@ export class Adjustment {
         this.l = [];
         this.l0 = [];
         this.p = [];
-        this.l_angles = [];
-        this.x_angles = [];
-        this.x_measurements = {};
-        this.l_measurements = {};
-        this.x_points = {};
-        this.l_points = {};
+        this.l_isAngle = [];
+        this.x_isAngle = [];
+        this.x_measurements = [];
+        this.l_measurements = [];
+        this.x_points = [];
+        this.l_points = [];
+        this.x_points_pos = {};
+        this.x_measurements_pos = {};
+        this.x_filter = [];
+        this.l_filter = [];
 
         for (let nr in this.points) {
             let c = this.points[nr].getCoordinate();
             if (!c) continue;
-            this.x_points[nr] = {};
+            this.x_points_pos[nr] = {};
             if (c.x !== undefined) {
                 this.x0.push(c.x);
-                this.x_points[nr].x = this.x0.length - 1;
+                this.x_points_pos[nr].x = this.x0.length - 1;
+                this.x_points.push({ nr: nr, component: 'x' });
+                this.x_isAngle.push(false);
             }
             if (c.y !== undefined) {
                 this.x0.push(c.y);
-                this.x_points[nr].y = this.x0.length - 1;
+                this.x_points_pos[nr].y = this.x0.length - 1;
+                this.x_points.push({ nr: nr, component: 'y' });
+                this.x_isAngle.push(false);
             }
             if (c.z !== undefined) {
                 this.x0.push(c.z);
-                this.x_points[nr].z = this.x0.length - 1;
+                this.x_points_pos[nr].z = this.x0.length - 1;
+                this.x_points.push({ nr: nr, component: 'z' });
+                this.x_isAngle.push(false);
             }
         }
 
@@ -75,42 +90,47 @@ export class Adjustment {
                 const entry: { o?: number, ih?: number } = {};
                 this.x0.push(gonToRad(m.orientation ?? 0));
                 entry.o = this.x0.length - 1;
-                this.x_angles.push(this.x0.length - 1);
-                //this.x0.push(m.instrumentHeight ?? 0)
-                //entry.ih = this.x0.length - 1;
+                this.x_isAngle.push(true);
+                this.x_measurements[i] = { measure: i, component: 'o' };
+/*
+                if (m.instrumentHeight !== undefined) {
+                    this.x0.push(m.instrumentHeight);
+                    this.x_isAngle.push(false);
+                    entry.ih = this.x0.length - 1;
+                    this.x_measurements[i] = { measure: i, component: 'ih' };
 
-                /*
-                m.measures.forEach((measure) => {
-                     x.push(measure.targetHeight);
-                }
-                */
-                this.x_measurements[i] = entry;
+                }   */
+                this.x_measurements_pos[i] = entry;
             }
         });
         return this.x0
     }
 
-
+    private filterImpossibleMeasurements() {
+        let A_sum = sum(this.A, 0) as unknown as number[];
+        this.x_filter = A_sum.map((x) => x === 0)
+        let A_sum2 = sum(this.A, 1) as unknown as number[];
+        this.l_filter = A_sum2.map((x) => x === 0)
+        return this.x_filter, this.l_filter;
+    }
 
     private createA_l0_dl_l() {
         this.A = [];
         this.l = [];
         this.l0 = [];
         this.p = [];
-        this.l_angles = [];
+        this.l_isAngle = [];
 
         this.measurements.forEach((m, i) => {
-            this.l_measurements[i] = {};
             if (m.type == 'theodolite' && m instanceof TheodoliteMeasure) {
-                const s = this.x_points[m.pointNumber];
+                const s = this.x_points_pos[m.pointNumber];
                 const sx = s.x;
                 const sy = s.y;
                 const sz = s.z;
                 if (sx === undefined || sy === undefined) return;
                 m.measures.forEach((measure, n) => {
-                    this.l_measurements[i][n] = {};
                     if (measure.active === false) return;
-                    const t = this.x_points[measure.nr];
+                    const t = this.x_points_pos[measure.nr];
                     const tx = t.x;
                     const ty = t.y;
                     const tz = t.z;
@@ -120,7 +140,7 @@ export class Adjustment {
                     const dist2 = dx * dx + dy * dy;
                     const dist = Math.sqrt(dist2);
                     // Hz
-                    if (measure.hz !== undefined && this.x_measurements[i].o !== undefined) {
+                    if (measure.hz !== undefined && this.x_measurements_pos[i].o !== undefined) {
                         const row = new Array(this.x0.length).fill(0);
 
                         row[sx] = - dy / dist2 * this.RHO;
@@ -133,29 +153,29 @@ export class Adjustment {
                         row[tx] = -dy / (dist * dist);
                         row[ty] = dx / (dist * dist);
                         */
-                        row[this.x_measurements[i].o] = -1;
+                        row[this.x_measurements_pos[i].o] = -1;
                         let azi = azimuth({ x: this.x0[sx], y: this.x0[sy] }, { x: this.x0[tx], y: this.x0[ty] });
-                        azi -= this.x0[this.x_measurements[i].o];
+                        azi -= this.x0[this.x_measurements_pos[i].o];
                         azi = gonBetween0And400(azi);
                         this.l0.push(azi);
                         this.l.push(measure.hz);
-                        this.l_measurements[i][n].hz = this.l.length - 1;
-                        this.l_angles.push(this.l.length - 1);
+                        this.l_measurements.push({ measure: i, entry: n, component: 'hz' });
+                        this.l_isAngle.push(true);
                         this.p.push(0.00025);
                         this.A.push(row);
                     }
                     // V
-                    if (measure.v !== undefined && sz !== undefined && tz !== undefined && this.x_measurements[i].ih !== undefined) {
+                    if (measure.v !== undefined && sz !== undefined && tz !== undefined && this.x_measurements_pos[i].ih !== undefined) {
                         const row = new Array(this.x0.length).fill(0);
                         const dz = this.x0[tz] - this.x0[sz];
-                        const ih = this.x0[this.x_measurements[i].ih];
+                        const ih = this.x0[this.x_measurements_pos[i].ih];
                         row[sz] = -1;
                         row[tz] = 1;
-                        row[this.x_measurements[i].ih] = -1;
+                        row[this.x_measurements_pos[i].ih] = -1;
                         this.l0.push(zenithDistance(dist - ih + (measure.targetHeight ?? 0), dz));
                         this.l.push(measure.v);
-                        this.l_measurements[i][n].v = this.l.length - 1;
-                        this.l_angles.push(this.l.length - 1);
+                        this.l_measurements.push({ measure: i, entry: n, component: 'v' });
+                        this.l_isAngle.push(true);
                         this.p.push(0.00025);
                         this.A.push(row);
                     }
@@ -167,7 +187,8 @@ export class Adjustment {
                         row[tx] = dx / dist;
                         row[ty] = dy / dist;
                         this.l0.push(dist);
-                        this.l_measurements[i][n].dist = this.l.length - 1;
+                        this.l_measurements.push({ measure: i, entry: n, component: 'dist' });
+                        this.l_isAngle.push(false);
                         this.l.push(measure.distance);
                         this.p.push(0.005);
                         this.A.push(row);
@@ -182,31 +203,33 @@ export class Adjustment {
             if (['resection', 'free_station', 'intersection', 'theodolite', 'adjustment', 'unknown'].includes(c.source)) continue;
         // else: 'manual' | 'gps' | 'map' | 'import' | 'transform' 
 
-            this.l_points[nr] = {};
-            if (c.x !== undefined && this.x_points[nr].x !== undefined) {
+            if (c.x !== undefined && this.x_points_pos[nr].x !== undefined) {
                 const row = new Array(this.x0.length).fill(0);
-                row[this.x_points[nr].x] = 1;
-                this.l0.push(this.x0[this.x_points[nr].x]);
+                row[this.x_points_pos[nr].x] = 1;
+                this.l0.push(this.x0[this.x_points_pos[nr].x]);
                 this.l.push(c.x);
-                this.l_points[nr].x = this.l.length - 1;
+                this.l_points.push({ nr: nr, component: 'x' });
+                this.l_isAngle.push(false);
                 this.p.push(c.accuracy);
                 this.A.push(row);
             }
-            if (c.y !== undefined && this.x_points[nr].y !== undefined) {
+            if (c.y !== undefined && this.x_points_pos[nr].y !== undefined) {
                 const row = new Array(this.x0.length).fill(0);
-                row[this.x_points[nr].y] = 1;
-                this.l0.push(this.x0[this.x_points[nr].y]);
+                row[this.x_points_pos[nr].y] = 1;
+                this.l0.push(this.x0[this.x_points_pos[nr].y]);
                 this.l.push(c.y);
-                this.l_points[nr].y = this.l.length - 1;
+                this.l_points.push({ nr: nr, component: 'y' });
+                this.l_isAngle.push(false);
                 this.p.push(c.accuracy);
                 this.A.push(row);
             }
-            if (c.z !== undefined && this.x_points[nr].z !== undefined) {
+            if (c.z !== undefined && this.x_points_pos[nr].z !== undefined) {
                 const row = new Array(this.x0.length).fill(0);
-                row[this.x_points[nr].z] = 1;
-                this.l0.push(this.x0[this.x_points[nr].z]);
+                row[this.x_points_pos[nr].z] = 1;
+                this.l0.push(this.x0[this.x_points_pos[nr].z]);
                 this.l.push(c.z);
-                this.l_points[nr].z = this.l.length - 1;
+                this.l_points.push({ nr: nr, component: 'z' });
+                this.l_isAngle.push(false);
                 this.p.push(c.accuracy);
                 this.A.push(row);
             }
@@ -214,10 +237,12 @@ export class Adjustment {
 
         this.dl = subtract(this.l, this.l0);
 
-        this.l_angles.forEach((x) => {
-            this.dl[x] = gonBetweenMinus200And200(this.dl[x]);
+        this.l_isAngle.forEach((x, i) => {
+            if (x) {
+                this.dl[i] = gonBetweenMinus200And200(this.dl[i]);
+            }
         });
-
+        this.filterImpossibleMeasurements();
         return this.A;
     }
 
@@ -229,7 +254,6 @@ export class Adjustment {
 
         for (let i = 0; i < 10; i++) {
             this.createA_l0_dl_l();
-
             this.P = inv(diag(this.p.map((x) => x * x))) as unknown as number[][];
             const At = transpose(this.A);
             const N = multiply(multiply(At, this.P), this.A);
@@ -239,9 +263,12 @@ export class Adjustment {
             this.dx = multiply(this.Qx, n) as unknown as number[];
             this.x0 = add(this.x0, this.dx) as number[]
 
-            this.x_angles.forEach((id) => {
-                this.x0[id] = gonBetween0And400(this.x0[id]);
-                this.dx[id] = gonBetweenMinus200And200(this.dx[id]);
+
+            this.x_isAngle.forEach((x, i) => {
+                if (x) {
+                    this.x0[i] = gonBetween0And400(this.x0[i]);
+                    this.dx[i] = gonBetweenMinus200And200(this.dx[i]);
+                }
             });
 
             console.log('run ', i, ', sum:', norm(this.dx));
